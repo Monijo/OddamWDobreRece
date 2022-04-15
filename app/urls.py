cd53873b6ffb2c528aefaecf1236df6342d55ddd
Monijo <mokajozefowicz@gmail.com>
2022-04-15 22:28:51 +0200

    Header optimization.

---
+++ app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+from .views import LandingPage, AddDonation, Login, Register
+
+app_name = 'app'
+
+urlpatterns = [
+    path('', LandingPage.as_view(), name='landingPage'),
+    path('addDonation', AddDonation.as_view(), name='addDonation'),
+    path('signIn', Login.as_view(), name='loginPage'),
+    path('signUp', Register.as_view(), name='registerPage'),
+]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class LandingPage(View):
+    def get(self, request):
+        return render(request, "app/index.html")
+
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, "app/form.html")
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, "app/login.html")
+
+
+class Register(View):
+    def get(self, request):
+        return render(request, "app/register.html")
